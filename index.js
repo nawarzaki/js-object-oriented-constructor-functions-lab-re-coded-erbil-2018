d561b2b1433f448ad6fd1fc38c0752e0c2b18c3c
nawaar.zakee <nawaar.zakee@yahoo.com>
2018-12-20 06:46:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ function Scooter(year, color, model) {
 }
 
   let samson = new Scooter(2015, "red", "swoosh")
+
+  function Driver(name, age, experience){
+    this.name = name
+    this.age = age
+    this.experience = experience
+  }
+    let allison = new Driver("Allison", "16 years", "2 years")
